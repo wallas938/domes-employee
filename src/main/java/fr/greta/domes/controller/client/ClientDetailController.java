95832d6947f315f65b09b62a1983cba791b298b1
wallas938 <drame.sissako001@gmail.com>
2023-02-01 15:50:54 +0100

    display client detail

--- src/main/java/fr/greta/domes/controller/client/ClientDetailController.java
+++ src/main/java/fr/greta/domes/controller/client/ClientDetailController.java
@@ -4,7 +4,10 @@ import fr.greta.domes.controller.NavigationController;
 import fr.greta.domes.model.Navigation;
 import fr.greta.domes.model.animal.Animal;
 import fr.greta.domes.model.client.Client;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,13 +15,35 @@ import java.util.ResourceBundle;
 public class ClientDetailController implements Initializable {
 
     private static Client currentClient;
+    @FXML
+    private Label registrationDate;
+    @FXML
+    private Label clientAddress;
+    @FXML
+    private Label clientEmail;
+    @FXML
+    private Label clientPhoneNumber;
+    @FXML
+    private Label clientFirstname;
+    @FXML
+    private Label clientLastname;
+    @FXML
+    private Label clientId;
+    @FXML
+    private Label orderNumberOfArticles;
+    @FXML
+    private Label orderReference;
+    @FXML
+    private Label purchaseDate;
+    @FXML
+    private Button showOrderDetail;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setFields(currentClient);
         NavigationController.getCurrentNavigation().addListener((observable, oldValue, newValue) -> {
 //            if (newValue.equals(Navigation.TO_CLIENT_DETAIL))
-                setFields(currentClient);
+            setFields(currentClient);
         });
     }
 
@@ -27,19 +52,104 @@ public class ClientDetailController implements Initializable {
     }
 
     public void setFields(Client currentClient) {
-//        setAnimalRef(currentAnimal.getId().toString());
-//        setAge(currentAnimal.getAge());
-//        setPrice(currentAnimal.getPrice());
-//        setDescription(currentAnimal.getDescription());
-//        setMainPicture(currentAnimal.getMainPicture());
-//        setSecondImage(currentAnimal.getSecondPicture());
-//        setThirdImage(currentAnimal.getThirdPicture());
+
+        setClientId(currentClient.getId().toString());
+        setClientLastname(currentClient.getLastname());
+        setClientFirstname(currentClient.getFirstname());
+        setClientAddress(currentClient.getAddress());
+        setClientPhoneNumber(currentClient.getPhoneNumber());
+        setClientEmail(currentClient.getEmail());
+        setRegistrationDate(currentClient.getRegistrationDate().toString());
 //        setFourthImage(currentAnimal.getFourthPicture());
 //        setCategory(currentAnimal.getCategory().getName());
 //        setSpecie(currentAnimal.getSpecie().getName());
     }
 
-    private void setClientId(String id) {
+    public static Client getCurrentClient() {
+        return currentClient;
+    }
 
+    public Label getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate.setText(registrationDate);
+    }
+
+    public Label getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress.setText(clientAddress);
+    }
+
+    public Label getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail.setText(clientEmail);
+    }
+
+    public Label getClientPhoneNumber() {
+        return clientPhoneNumber;
+    }
+
+    public void setClientPhoneNumber(String clientPhoneNumber) {
+        this.clientPhoneNumber.setText(clientPhoneNumber);
+    }
+
+    public Label getClientFirstname() {
+        return clientFirstname;
+    }
+
+    public void setClientFirstname(String clientFirstname) {
+        this.clientFirstname.setText(clientFirstname);
+    }
+
+    public Label getClientLastname() {
+        return clientLastname;
+    }
+
+    public void setClientLastname(String clientLastname) {
+        this.clientLastname.setText(clientLastname);
+    }
+
+    public Label getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId.setText(clientId);
+    }
+
+    public Label getOrderNumberOfArticles() {
+        return orderNumberOfArticles;
+    }
+
+    public void setOrderNumberOfArticles(int orderNumberOfArticles) {
+        this.orderNumberOfArticles.setText(String.valueOf(orderNumberOfArticles));
+    }
+
+    public Label getOrderReference() {
+        return orderReference;
+    }
+
+    public void setOrderReference(String orderReference) {
+        this.orderReference.setText(orderReference);
+    }
+
+    public Label getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate.setText(purchaseDate);
+    }
+
+    public Button getShowOrderDetail() {
+        return showOrderDetail;
     }
 }
